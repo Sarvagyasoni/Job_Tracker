7f22a98128a37fa0289c22d15920c569384c0b5e
Sarvagya Soni <12sarvagyasoni@gmail.com>
2026-08-25 13:52:00 +0530

    Delete job-tracker-frontend directory

--- job-tracker-frontend/src/components/common/index.ts
+++
@@ -1,7 +0,0 @@
-export { Button } from './Button';
-export { Input } from './Input';
-export { Select } from './Select';
-export { Textarea } from './Textarea';
-export { Card, CardHeader, CardContent, CardFooter } from './Card';
-export { Modal } from './Modal';
-export { ToastProvider, useToast, type Toast, type ToastType } from './Toast';
--- job-tracker-frontend/src/components/jobs/index.ts
+++
@@ -1,7 +0,0 @@
-export { JobCard } from './JobCard';
-export { JobForm } from './JobForm';
-export { JobList } from './JobList';
-export { KanbanBoard } from './KanbanBoard';
-export { KanbanColumn } from './KanbanColumn';
-export { JobSearch } from './JobSearch';
-export { JobSearchResultCard } from './JobSearchResultCard';
